0411010e8e80e9ec5304309036a0063fd08cba92
Full Stack Developer GB <163743268+fsdcoder@users.noreply.github.com>
2024-09-10 21:27:22 +0530

    Add files via upload

---
+++ Add-to-cart/src/dummy-products.js
@@ -0,0 +1,58 @@
+import mochaOvercoat from './assets/mocha-overcoat.jpg';
+import dreamGown from './assets/dream-gown.jpg';
+import rainJacket from './assets/rain-jacket.jpg';
+import merlotSuit from './assets/merlot-suit.jpg';
+import moonlightDress from './assets/moonlight-dress.jpg';
+import denimPioneer from './assets/denim-pioneer.jpg';
+
+export const DUMMY_PRODUCTS = [
+  {
+    id: '1',
+    image: mochaOvercoat,
+    title: 'Product 1',
+    price: 10,
+    description:
+      'Desciption 1',
+  },
+  {
+    id: '2',
+    image: dreamGown,
+    title: 'Product 1',
+    price: 20,
+    description:
+      'Desciption 2',
+  },
+
+  {
+    id: '3',
+    image: rainJacket,
+    title: 'Product 3',
+    price: 30,
+    description:
+      'Desciption 3',
+  },
+  {
+    id: '4',
+    image: merlotSuit,
+    title: 'Product 4',
+    price: 40,
+    description:
+    'Desciption 4',
+    },
+    {
+    id: '5',
+    image: moonlightDress,
+    title: 'Product 5',
+    price: 50,
+    description:
+    'Desciption 5',
+    },
+    {
+    id: '6',
+    image: denimPioneer,
+    title: 'Product 6',
+    price: 60,
+    description:
+    'Desciption 6',
+    }
+];
